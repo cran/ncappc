0d01e63601de3ffe40da5e1f4cf28a38cf0cbdde
Chayan Acharya <chayan.acharya@farmbio.uu.se>
2015-03-04 16:14:48 +0000

    version 0.1

---
+++ R/calc.stat.R
@@ -0,0 +1,51 @@
+# R function to calculate a set of statistics for a given array of numbers
+# Chayan, 12/2014
+
+# roxygen comments
+#' Calculates a set of statistics for a given array of numbers.
+#'
+#' \pkg{calc.stat} calculates a set of statistics for a given array of numbers.
+#'
+#' \pkg{calc.stat} calculates a set of statistics for a given array of numbers.
+#' The calculated statistics are
+#' \itemize{
+#'  \item Ntot = length of the array
+#'  \item Nunique = Number of unique elements
+#'  \item Min = Minimum value of the array
+#'  \item Max = Maximum value of the array
+#'  \item Mean = Mean value of the array
+#'  \item Median = Median value of the array
+#'  \item SD = Standard deviation value of the array
+#'  \item SE = Standard error value of the array
+#'  \item CVp = Percent coefficient of variation of the array
+#'  \item CI95u = Upper limit of the 95\% confidence interval of the array
+#'  \item CI95l = Lower limit of the 95\% confidence interval of the array
+#'  \item gMean = Geometric mean value of the array
+#'  \item gCVp = Geometric percent coefficient of variation of the array
+#' }
+#' 
+#' @param x a numeric array
+#'
+#' @return An array of calculated statistics of a given set of numbers
+#' @export
+#'
+
+calc.stat <- function(x){
+  x       <- as.numeric(x)
+  Ntot    <- length(x)
+  Nunique <- length(unique(x))
+  Min     <- min(x)
+  Max     <- max(x)
+  Mean    <- mean(x)
+  Median  <- median(x)
+  SD      <- sd(x)
+  SE      <- SD/sqrt(Ntot)
+  CVp     <- 100*SD/Mean
+  CI95l   <- Mean-abs(qt(0.025,Ntot-1)*SE)  # two-tailed
+  CI95u   <- Mean+abs(qt(0.975,Ntot-1)*SE)  # two-tailed
+  gMean   <- exp(mean(log(x)))
+  gCVp    <- sqrt(exp(sd(log(x))^2)-1)*100
+  stPrm   <- c(Ntot,Nunique,Min,Max,Mean,Median,SD,SE,CVp,CI95l,CI95u,gMean,gCVp)
+  names(stPrm) <- c("Ntot","Nunique","Min","Max","Mean","Median","SD","SE","CVp","CI95l","CI95u","gMean","gCVp")
+  return(stPrm)
+}
---
+++ R/dv.plot.R
@@ -0,0 +1,47 @@
+# Plot DV vs Time data
+
+# roxygen comments
+#' Plots drug plasma concentration vs time data
+#'
+#' \pkg{dv.plot} plots DV vs Time data.
+#'
+#' \pkg{dv.plot} plots DV vs Time data
+#' 
+#' @param pdata A data frame with three mandatory columns and one optional
+#'   column:
+#' \enumerate{
+#'  \item Time: Time column (mandatory)
+#'  \item Conc: DV column (mandatory)
+#'  \item ID: Individual ID (mandatory)
+#'  \item FCT: Dose identifier and/or population stratifier (optional)
+#' }
+#' @param cunit Unit for concentration (\strong{"[M].[L]^-3"})
+#' @param tunit Unit for time (\strong{"[T]"})
+#'
+#' @return returns a graphical object created by arrangeGrob function
+#' @export
+#'
+
+dv.plot <- function(pdata,cunit="[M].[L]^-3",tunit="[T]"){
+  ID <- Time <- Conc <- NULL
+  rm(list=c("ID","Time","Conc"))
+  
+  ggOpt_conc <- list(theme(plot.margin = unit(c(0.2,0.2,0.2,0.2), "cm"),
+                           panel.margin = unit(0.2, "cm"),
+                           axis.text.x  = element_text(size=9,face="bold",color="black",vjust=1,hjust=0.5),
+                           axis.text.y  = element_text(size=9,face="bold",color="black",hjust=0),
+                           legend.position = "none",
+                           strip.text.x = element_text(size=9, face="bold")),
+                     xlab(""),ylab(""),geom_line(aes(colour=factor(ID)), size=0.5),geom_point(aes(colour=factor(ID)), size=1))
+  p01 <- ggplot(pdata, aes(x=as.numeric(as.character(Time)), y=as.numeric(as.character(Conc)), color=factor(ID))) + ggOpt_conc
+  
+  if ("FCT"%in%names(pdata)) p01 <- p01 + facet_wrap(~FCT, scales="free", ncol=1)
+  
+  p02 <- p01 + scale_y_log10()
+  gdr <- suppressMessages(suppressWarnings(
+    arrangeGrob(p01,p02,ncol=2,
+                main=textGrob("Concentration vs. Time profile\n",vjust=1,gp=gpar(fontface="bold",cex = 0.7)),
+                left=textGrob(paste("\nPlasma concentration (",cunit,")",sep=""),gp=gpar(fontface="bold",cex = 0.7),rot=90),
+                sub=textGrob(paste("Time (",tunit,")\n"),gp=gpar(fontface="bold",cex = 0.7)))))
+  return(gdr)
+}
